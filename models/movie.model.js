2f3b4262e386ef215cac12dc6d7cc8242dd2033e
Akhilleshgoswami <agiri5375@gmail.com>
2026-04-17 13:57:02 +0530

    Feature : add movie model

---
+++ models/movie.model.js
@@ -0,0 +1,47 @@
+
+const mongoose = require('mongoose')
+
+/**
+ * Define the schema of movie resource to be stored in the db 
+ *
+ */
+const movieSchema = new mongoose.Schema({
+
+ name: {
+  type: String,
+  required: true
+ },
+ description: {
+  type: String,
+  required: true
+ },
+ casts: {
+  type: [String],
+  required: true
+ },
+ trailerUrl: {
+  type: String,
+  required: true,
+ },
+ langage: {
+  type: String,
+  required: true,
+  default: "English"
+ },
+ releaseDate: {
+  type: String,
+  required: true
+ },
+ director: {
+  type: String,
+  required: true
+ },
+ releaseStatus: {
+  type: String,
+  required: true,
+  default: "RELEASED"
+ }
+}, { timestamps: true })
+
+const movie = mongoose.model("Movie", movieSchema)// create a new model
+module.exports = movie // returning model
