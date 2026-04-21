b100f6e9e93eab632f2502e85986c95d8ed140ad
Akhilleshgoswami <agiri5375@gmail.com>
2026-04-21 13:34:22 +0530

    feat: Added an api if an movie is runing in a theater or not

--- controllers/theater.controller.js
+++ controllers/theater.controller.js
@@ -1,79 +1,79 @@
 const Theater = require("../models/theatre.model");
 const TheaterService = require("../services/theater.service")
-const {successResponseBody,errorResponseBody} = require("../utils/responseBody")
-const create = async(req,res)=>{
+const { successResponseBody, errorResponseBody } = require("../utils/responseBody")
+const create = async (req, res) => {
  try {
   const result = await TheaterService.createTheater(req.body);
-  if(result.err){
-   errorResponseBody.error =  result.err
+  if (result.err) {
+   errorResponseBody.error = result.err
    errorResponseBody.message = "Validation failed on few parameters of the request body"
    return res.status(result.code).json(errorResponseBody)
   }
   successResponseBody.data = result;
   return res.status(201).json(successResponseBody)
- }catch(error){
-  console.log("error while createing theater",error)
+ } catch (error) {
+  console.log("error while createing theater", error)
   errorResponseBody.error = error;
   return res.status(500).json(errorResponseBody)
 
  }
 }
-const getTheater = async(req,res)=>{
- try{
- const result = await TheaterService.getDocById(req.params.id);
- if(result.err){
-   errorResponseBody.error =  result.err
+const getTheater = async (req, res) => {
+ try {
+  const result = await TheaterService.getDocById(req.params.id);
+  if (result.err) {
+   errorResponseBody.error = result.err
    return res.status(result.code).json(errorResponseBody)
- }
+  }
   successResponseBody.data = result;
   return res.status(200).json(successResponseBody)
- }catch(error){
+ } catch (error) {
   errorResponseBody.error = error;
   return res.status(500).json(errorResponseBody)
  }
 }
 
-const deleteTheater = async(req,res)=>{
- try{
- const result = await TheaterService.deleteTheaterd(req.params.id);
+const deleteTheater = async (req, res) => {
+ try {
+  const result = await TheaterService.deleteTheaterd(req.params.id);
   successResponseBody.data = result;
   return res.status(200).json(successResponseBody)
- }catch(error){
+ } catch (error) {
   errorResponseBody.error = error;
   return res.status(500).json(errorResponseBody)
  }
 }
 
-const fetchTheater= async(req,res) =>{
- try{
-const result = await TheaterService.fetchTheater(req.query)
-  if(result.err){
+const fetchTheater = async (req, res) => {
+ try {
+  const result = await TheaterService.fetchTheater(req.query)
+  if (result.err) {
    errorResponseBody.error = result.err;
    return res.status(result.code).json(errorResponseBody);
   }
   successResponseBody.data = result;
   return res.status(200).json(successResponseBody);
 
- }catch(error){
-  console.log("error",error)
+ } catch (error) {
+  console.log("error", error)
   errorResponseBody.error = error
   return res.status(500).json(
    errorResponseBody
   )
  }
 }
-const updateMovies = async(req,res)=>{
- try{
-const result =  await TheaterService.updateMoviesInTheaters(req.params.id,req.body.movieIds,req.body.insert)
-if(result.err){
+const updateMovies = async (req, res) => {
+ try {
+  const result = await TheaterService.updateMoviesInTheaters(req.params.id, req.body.movieIds, req.body.insert)
+  if (result.err) {
    errorResponseBody.error = result.err;
    return res.status(result.code).json(errorResponseBody);
   }
   successResponseBody.data = result;
   return res.status(200).json(successResponseBody);
 
- }catch(error){
-  console.log("error",error)
+ } catch (error) {
+  console.log("error", error)
   errorResponseBody.error = error
   return res.status(500).json(
    errorResponseBody
@@ -81,13 +81,13 @@ if(result.err){
  }
 }
 
-const fetchAllTheater = async(req,res) =>{
- try{
-const result = await TheaterService.getAllTheaters(req.query)
+const fetchAllTheater = async (req, res) => {
+ try {
+  const result = await TheaterService.getAllTheaters(req.query)
   successResponseBody.data = result;
   return res.status(200).json(successResponseBody);
 
- }catch(error){
+ } catch (error) {
   errorResponseBody.error = error
   return res.status(500).json(
    errorResponseBody
@@ -96,25 +96,43 @@ const result = await TheaterService.getAllTheaters(req.query)
 }
 
 
-const getMovies = async(req,res) =>{
- try{
- const result = await TheaterService.getMoviesInATheater(req.params.id)
- if(result.err){
-   errorResponseBody.error =  result.err
+const getMovies = async (req, res) => {
+ try {
+  const result = await TheaterService.getMoviesInATheater(req.params.id)
+  if (result.err) {
+   errorResponseBody.error = result.err
    errorResponseBody.message = "Validation failed on few parameters of the request body"
    return res.status(result.code).json(errorResponseBody)
   }
   successResponseBody.data = result;
   return res.status(200).json(successResponseBody);
 
- }catch(error){
+ } catch (error) {
   errorResponseBody.error = error
   return res.status(500).json(
    errorResponseBody
   )
  }
 }
+const checkMovie = async (req, res) => {
+
+ try {
+  const result = await TheaterService.checkMovieInAtheater(req.params.theaterId, req.params.movieId)
+  if (result.err) {
+   errorResponseBody.error = result.err
+   return res.status(result.code).json(errorResponseBody)
+  }
+
+  successResponseBody.data = result;
+  return res.status(200).json(successResponseBody);
+ } catch (error) {
+  errorResponseBody.error = error
+  return res.status(500).json(
+   errorResponseBody)
+ }
+
+}
 module.exports = {
  create,
- getTheater,deleteTheater,fetchTheater,updateMovies,fetchAllTheater,getMovies
+ getTheater, deleteTheater, fetchTheater, updateMovies, fetchAllTheater, getMovies,checkMovie
 }
--- routes/theater.routes.js
+++ routes/theater.routes.js
@@ -1,13 +1,14 @@
 
 const TheaterController = require("../controllers/theater.controller")
 const TheaterMiddlerWare = require("../middlewares/theater.middlewares")
-const router = (app)=>{
- app.post("/mba/api/v1/theater",TheaterMiddlerWare.validateTheaterCreateRequest, TheaterController.create)
- app.get("/mba/api/v1/theater/:id",TheaterController.getTheater)
- app.delete("/mba/api/v1/theater/:id",TheaterController.deleteTheater)
- app.put("/mba/api/v1/theater/:id/movies",TheaterController.updateMovies)
- app.get("/mba/api/v1/theater/:id/movies",TheaterController.getMovies)
- app.get("/mba/api/v1/theater",TheaterController.fetchAllTheater)
+const router = (app) => {
+ app.post("/mba/api/v1/theater", TheaterMiddlerWare.validateTheaterCreateRequest, TheaterController.create)
+ app.get("/mba/api/v1/theater/:id", TheaterController.getTheater)
+ app.delete("/mba/api/v1/theater/:id", TheaterController.deleteTheater)
+ app.put("/mba/api/v1/theater/:id/movies", TheaterController.updateMovies)
+ app.get("/mba/api/v1/theater/:id/movies", TheaterController.getMovies)
+ app.get("/mba/api/v1/theater", TheaterController.fetchAllTheater)
+ app.get("/mba/api/v1/theater/:theaterId/movies/:movieId", TheaterController.checkMovie)
 }
 
 module.exports = router
