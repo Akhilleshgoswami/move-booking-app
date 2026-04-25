8e0d61e81e03d51f10eaf509511736817bce3b57
Akhilleshgoswami <agiri5375@gmail.com>
2026-04-25 11:23:41 +0530

    feat:Add payment schema

---
+++ models/payment.model.js
@@ -0,0 +1,23 @@
+
+const mongoose = require("mongoose")
+const paymentSchema= new mongoose.Schema({
+ bookingId:{
+  type:mongoose.Schema.Types.ObjectId,
+  required:true,
+  ref:"Booking"
+ },
+ amount:{
+  type:Number,
+  required:true
+ },
+ status:{
+  type:String,
+  required:true,
+  enum:{
+   values:["SUCCESS","FAILED","PENDING"]
+  },
+  default:PENDING
+ }
+},{timestamps:true})
+const Payment =  mongoose.model("Paymet",paymentSchema);
+module.exports  = Payment
